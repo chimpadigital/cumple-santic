104adea6957c9d471a84fd38b22afbe5e34326dd
stefanoracke <stefanoracke@gmail.com>
2024-06-27 10:30:16 -0300

    ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors:{
-        primary:"#73899E"
+        primary:"#73899E",
+        secondary: "#3C5771"
       }
     },
   },
